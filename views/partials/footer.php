cdc7ed1e3ba45b6164d0432815ff71c0a519a0d8
Sandesh <sandeshb981@gmail.com>
2022-10-28 21:33:01 +0200

    added titan slider code

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -5,5 +5,6 @@
 <script src="../../resources/modules/tera-lightbox/tera-lightbox.js"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138942259-1"></script>
 <script src="../../resources/scripts/custom.js"></script>
+<script>$('#img-carousel').titanSlider({carousel : true, autoplay : true});</script>
 </body>
 </html>
