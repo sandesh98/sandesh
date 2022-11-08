27c0e0e02dbd9adf71e8a6c768b8c86233dde642
Sandesh <sandeshb981@gmail.com>
2022-11-08 13:24:38 +0100

    added versioning for js and css documents

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,10 +1,10 @@
 </div>
-<script src="../../resources/scripts/vendor.js"></script>
-<script src="../../resources/scripts/functions.js"></script>
-<script src="../../resources/modules/titan-slider/titan-slider.js"></script>
-<script src="../../resources/modules/tera-lightbox/tera-lightbox.js"></script>
+<script src="<?= auto_version('/resources/scripts/vendor.js') ?>"></script>
+<script src="<?= auto_version('/resources/scripts/functions.js') ?>"></script>
+<script src="<?= auto_version('/resources/modules/titan-slider/titan-slider.js') ?>"></script>
+<script src="<?= auto_version('/resources/modules/tera-lightbox/tera-lightbox.js') ?>"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138942259-1"></script>
-<script src="../../resources/scripts/custom.js"></script>
+<script src="<?= auto_version('/resources/scripts/custom.js') ?>"></script>
 <script>$('#img-carousel').titanSlider({carousel : true, autoplay : true});</script>
 </body>
 </html>
