04f1dbd8abceed860012add660d67bcb403e7f95
Sandesh Boedhoe <sandeshb981@gmail.com>
2023-06-02 18:52:06 +0200

    Fixed play pause button with new logic

--- views/rs.view.php
+++ views/rs.view.php
@@ -7,9 +7,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-<!--    <link rel="preconnect" href="https://fonts.googleapis.com">-->
-<!--    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>-->
-<!--    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,700;1,500&display=swap" rel="stylesheet">
     <style>
         html, body {
             font-family: 'Poppins', sans-serif;
@@ -26,26 +26,105 @@
             border: none;
             padding: 0;
             font: inherit;
-            cursor: pointer;
             outline: inherit;
         }
     </style>
 </head>
 <body>
 <div class="container">
-    <h1 class="pt-5 pb-3">RS Soundboard</h1>
+    <h1 class="pt-5 pb-3 fw-bold">RS Soundboard</h1>
+
+<!--    <div class="card rounded-4 mb-4">-->
+<!--        <div class="card-body">-->
+<!--            <div class="d-flex align-items-center" style="gap: 24px">-->
+<!--                <div class="px-2">-->
+<!--                    <button onclick="playSound('sound1')" id="sound-button-1">-->
+<!--                        <i class="bi bi-play-fill" id="sound-icon-1" style="font-size: 2.4rem;"></i>-->
+<!--                    </button>-->
+<!--                </div>-->
+<!--                <div>-->
+<!--                    <h4 class="card-title mb-0">Audi RS6 Modded</h4>-->
+<!--                    <div class="card-text">Pops and bangs</div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+
+<!--    <div class="card rounded-4 mb-4">-->
+<!--        <div class="card-body">-->
+<!--            <div class="d-flex align-items-center" style="gap: 24px">-->
+<!--                <div class="px-2">-->
+<!--                    <button onclick="playSound('sound2')" id="sound-button-1">-->
+<!--                        <i class="bi bi-play-fill" id="sound-icon-1" style="font-size: 2.4rem;"></i>-->
+<!--                    </button>-->
+<!--                </div>-->
+<!--                <div>-->
+<!--                    <h4 class="card-title mb-0">Audi RS6 Startup</h4>-->
+<!--                    <div class="card-text">Startup</div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!---->
+<!--    <div class="card rounded-4 mb-4">-->
+<!--        <div class="card-body">-->
+<!--            <div class="d-flex align-items-center" style="gap: 24px">-->
+<!--                <div class="px-2">-->
+<!--                    <button onclick="playSound('sound3')" id="sound-button-1">-->
+<!--                        <i class="bi bi-play-fill" id="sound-icon-1" style="font-size: 2.4rem;"></i>-->
+<!--                    </button>-->
+<!--                </div>-->
+<!--                <div>-->
+<!--                    <h4 class="card-title mb-0">Audi RSQ8</h4>-->
+<!--                    <div class="card-text">Launchcontrol</div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+
+<!--    <div class="card rounded-4 mb-4">-->
+<!--        <div class="card-body">-->
+<!--            <div class="d-flex align-items-center" style="gap: 24px">-->
+<!--                <div class="px-2">-->
+<!--                    <button onclick="playSound('sound1')">-->
+<!--                        <i class="bi bi-play-fill" id="sound1" style="font-size: 2.4rem;"></i>-->
+<!--                    </button>-->
+<!--                </div>-->
+<!--                <div>-->
+<!--                    <h4 class="card-title mb-0">Audi RS6</h4>-->
+<!--                    <div class="card-text">Pops and bangs</div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 
     <div class="card rounded-4 mb-4">
         <div class="card-body">
             <div class="d-flex align-items-center" style="gap: 24px">
                 <div class="px-2">
-                    <button onclick="playSound('sound1')">
-                        <i class="bi bi-play-fill" id="sound1" style="font-size: 2.4rem;"></i>
+                    <button onclick="togglePlayPause(0)">
+                        <i class="bi bi-play-fill" id="sound-icon-0" style="font-size: 2.4rem;"></i>
                     </button>
                 </div>
                 <div>
-                    <h4 class="card-title mb-0">Audi RS6</h4>
+                    <h4 class="card-title mb-0">Audi RS6 Modded</h4>
                     <div class="card-text">Pops and bangs</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card rounded-4 mb-4">
+        <div class="card-body">
+            <div class="d-flex align-items-center" style="gap: 24px">
+                <div class="px-2">
+                    <button onclick="togglePlayPause(1)">
+                        <i class="bi bi-play-fill" id="sound-icon-1" style="font-size: 2.4rem;"></i>
+                    </button>
+                </div>
+                <div>
+                    <h4 class="card-title mb-0">Audi RSQ8</h4>
+                    <div class="card-text">Launchcontrol</div>
                 </div>
             </div>
         </div>
@@ -57,39 +136,34 @@
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
 <script>
-    function playSound(sound) {
-        console.log(sound);
-        // setAnimation();
-        let sound1 = 'resources/sounds/audio1.m4a';
-        let sound2 = 'resources/sounds/piraat.mp3';
-        let sound3 = 'resources/sounds/piraat.mp3';
 
-        let audio = new Audio();
+    let audioElements = [];
+    let playPauseButtons = [];
 
-        switch (sound) {
-            case 'sound1':
-                audio.src = sound1;
-                break;
-            case 'sound2':
-                audio.src = sound2;
-                break;
-            case 'sound3':
-                audio.src = sound3;
-                break;
-            default:
-                console.log('Ongeldige geluidsnaam');
-                return;
-        }
-
-        audio.play();
-
+    for (let i = 0; i < 3; i++) {
+        let audio = new Audio('resources/sounds/' + (i + 1) + '.mp3');
+        audioElements.push(audio);
     }
 
-    function setAnimation() {
-        let icon = document.getElementById('#sound1');
-        console.log(icon);
+    function togglePlayPause(index) {
+        let audio = audioElements[index];
+        let button = playPauseButtons[index];
+        let icon = document.getElementById("sound-icon-" + index);
 
-        // console.log('animation');
+        if (audio.paused) {
+            audio.play();
+            icon.classList.remove("bi-play-fill");
+            icon.classList.add("bi-pause-fill");
+        } else {
+            audio.pause();
+            icon.classList.remove("bi-pause-fill");
+            icon.classList.add("bi-play-fill");
+        }
+
+        audio.onended = function() {
+            icon.classList.remove("bi-pause-fill");
+            icon.classList.add("bi-play-fill");
+        }
     }
 </script>
 </body>
