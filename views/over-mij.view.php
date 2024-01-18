a3cd901f9a48e4dc18c62874c0518ea8a8036e28
Sandesh Boedhoe <sandesh@MacBook-Air-van-Sandesh.local>
2024-01-18 11:24:19 +0100

    added end date for afstudeerstage

--- views/over-mij.view.php
+++ views/over-mij.view.php
@@ -153,7 +153,7 @@
                     <div class="list-block d-sm-none d-md-none"> <!-- alleen zichtbaar bij groter dan sm sreen -->
                         <h3 class="stripe">Werk</h3>
 
-                        <span class="label">Aug 2023 - Heden</span>
+                        <span class="label">Aug 2023 - Jan 2023</span>
                         <ul class="striped">
                             <li>JEX<br>
                                 UX/UI-Afstudeerstage
